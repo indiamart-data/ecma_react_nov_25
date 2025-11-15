15c2907cac2a02189ac7ae49098a10306cd9bf74
Manish Sharma <developer@technizeredge.com>
2025-11-15 12:49:46 +0530

    Multi Component

---
+++ Day3/react-core-app/src/components/2_multi-components/ComponentOne.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class ComponentOne extends Component {
+    render() {
+        return (
+            <h1 className='text-primary'>
+                Hello from Component One
+            </h1>
+        );
+    }
+}
+
+export default ComponentOne;
