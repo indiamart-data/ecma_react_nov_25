8421bfb9bee5f0f7030f2160b7cff1571722cfdc
Manish Sharma <developer@technizeredge.com>
2025-11-17 10:34:44 +0530

    Counter Class

--- Day4/react-core-app/src/components/assignment/CounterAssignment.jsx
+++ Day4/react-core-app/src/components/assignment/CounterAssignment.jsx
@@ -1,32 +1,83 @@
+import PropTypes from 'prop-types';
 import { Component } from 'react';
 
 class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { count: 0, flag: false };
+        this.clickCount = 0;
+        this.inc = this.inc.bind(this);
+        this.dec = this.dec.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    manageClickCount() {
+        this.clickCount += 1;
+        if (this.clickCount > 9) {
+            this.setState({ flag: true });
+        }
+    }
+
+    inc() {
+        this.setState(
+            (prev) => ({ count: prev.count + this.props.interval }),
+            () => this.manageClickCount());
+    }
+
+    dec() {
+        this.setState(
+            (prev) => ({ count: prev.count - this.props.interval }),
+            () => this.manageClickCount()
+        );
+    }
+
+    reset() {
+        this.clickCount = 0;
+        this.setState({ count: 0, flag: false });
+    }
+
     render() {
+        const { flag } = this.state;
+
         return (
             <>
                 <div className="text-center">
                     <h3 className="text-info">Counter Component</h3>
                 </div>
                 <div className="d-grid gap-2 mx-auto col-6">
-                    <input type="text" className="form-control form-control-lg" />
-                    <CounterControls />
+                    <input type="text" className="form-control form-control-lg" value={this.state.count} readOnly />
+                    <CounterControls
+                        flag={flag}
+                        inc={this.inc}
+                        dec={this.dec}
+                        reset={this.reset} />
                 </div>
             </>
         );
     }
 }
 
+Counter.propTypes = {
+    interval: PropTypes.number
+}
+
+Counter.defaultProps = {
+    interval: 1
+}
+
 class CounterControls extends Component {
     render() {
+        const { flag, inc, dec, reset } = this.props;
+
         return (
             <div className="d-grid gap-2">
-                <button className="btn btn-info">
+                <button className="btn btn-info" disabled={flag} onClick={inc}>
                     <span className="fs-4">+</span>
                 </button>
-                <button className="btn btn-info">
+                <button className="btn btn-info" disabled={flag} onClick={dec}>
                     <span className="fs-4">-</span>
                 </button>
-                <button className="btn btn-secondary">
+                <button className="btn btn-secondary" disabled={!flag} onClick={reset}>
                     <span className="fs-4">Reset</span>
                 </button>
             </div>
@@ -40,7 +91,7 @@ class CounterAssignment extends Component {
             <div>
                 <Counter />
                 <hr />
-                <Counter interval={10}/>
+                <Counter interval={10} />
             </div>
         );
     }
--- Day4/react-core-app/src/components/root/RootComponent.jsx
+++ Day4/react-core-app/src/components/root/RootComponent.jsx
@@ -1,9 +1,11 @@
-import EventComponent from "../1_react-events/EventComponent";
+// import EventComponent from "../1_react-events/EventComponent";
+import CounterAssignment from "../assignment/CounterAssignment";
 
 const RootComponent = () => {
     return (
         <div className='container'>
-            <EventComponent />
+            {/* <EventComponent /> */}
+            <CounterAssignment />
         </div>
     );
 };
