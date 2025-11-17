0750573c9c559c7e5411448fa9eb03ef3e82c9bd
Manish Sharma <developer@technizeredge.com>
2025-11-17 11:22:56 +0530

    Updated to Functional Component

--- Day4/react-core-app/src/components/assignment/CounterAssignment.jsx
+++ Day4/react-core-app/src/components/assignment/CounterAssignment.jsx
@@ -1,100 +1,204 @@
+// import PropTypes from 'prop-types';
+// import React, { Component } from 'react';
+
+// class Counter extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { count: 0, flag: false };
+//         this.clickCount = 0;
+//         this.inc = this.inc.bind(this);
+//         this.dec = this.dec.bind(this);
+//         this.reset = this.reset.bind(this);
+//     }
+
+//     manageClickCount() {
+//         this.clickCount += 1;
+//         if (this.clickCount > 9) {
+//             this.setState({ flag: true });
+//         }
+//     }
+
+//     inc() {
+//         this.setState(
+//             (prev) => ({ count: prev.count + this.props.interval }),
+//             () => this.manageClickCount());
+//     }
+
+//     dec() {
+//         this.setState(
+//             (prev) => ({ count: prev.count - this.props.interval }),
+//             () => this.manageClickCount()
+//         );
+//     }
+
+//     reset() {
+//         this.clickCount = 0;
+//         this.setState({ count: 0, flag: false });
+//     }
+
+//     render() {
+//         const { flag } = this.state;
+
+//         return (
+//             <>
+//                 <div className="text-center">
+//                     <h3 className="text-info">Counter Component</h3>
+//                 </div>
+//                 <div className="d-grid gap-2 mx-auto col-6">
+//                     <input type="text" className="form-control form-control-lg" value={this.state.count} readOnly />
+//                     <CounterControls
+//                         flag={flag}
+//                         inc={this.inc}
+//                         dec={this.dec}
+//                         reset={this.reset} />
+//                 </div>
+//             </>
+//         );
+//     }
+// }
+
+// Counter.propTypes = {
+//     interval: PropTypes.number
+// }
+
+// Counter.defaultProps = {
+//     interval: 1
+// }
+
+// // class CounterControls extends Component {
+// //     render() {
+// //         console.log("CounterControls Render Executed:" + JSON.stringify(this.props));
+// //         const { flag, inc, dec, reset } = this.props;
+
+// //         return (
+// //             <div className="d-grid gap-2">
+// //                 <button className="btn btn-info" disabled={flag} onClick={inc}>
+// //                     <span className="fs-4">+</span>
+// //                 </button>
+// //                 <button className="btn btn-info" disabled={flag} onClick={dec}>
+// //                     <span className="fs-4">-</span>
+// //                 </button>
+// //                 <button className="btn btn-secondary" disabled={!flag} onClick={reset}>
+// //                     <span className="fs-4">Reset</span>
+// //                 </button>
+// //             </div>
+// //         );
+// //     }
+// // }
+
+// const CounterControls = React.memo(class extends Component {
+//     render() {
+//         console.log("CounterControls Render Executed:" + JSON.stringify(this.props));
+//         const { flag, inc, dec, reset } = this.props;
+
+//         return (
+//             <div className="d-grid gap-2">
+//                 <button className="btn btn-info" disabled={flag} onClick={inc}>
+//                     <span className="fs-4">+</span>
+//                 </button>
+//                 <button className="btn btn-info" disabled={flag} onClick={dec}>
+//                     <span className="fs-4">-</span>
+//                 </button>
+//                 <button className="btn btn-secondary" disabled={!flag} onClick={reset}>
+//                     <span className="fs-4">Reset</span>
+//                 </button>
+//             </div>
+//         );
+//     }
+// });
+
+// class CounterAssignment extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Counter />
+//                 {/* <hr />
+//                 <Counter interval={10} /> */}
+//             </div>
+//         );
+//     }
+// }
+
+// export default CounterAssignment;
+
+// ----------------------------------------------------- Functional
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import React, { useRef, useState } from 'react';
 
-class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { count: 0, flag: false };
-        this.clickCount = 0;
-        this.inc = this.inc.bind(this);
-        this.dec = this.dec.bind(this);
-        this.reset = this.reset.bind(this);
-    }
+const Counter = ({ interval = 1 }) => {
+    const [count, setCount] = useState(0);
+    const [flag, setFlag] = useState(false);
 
-    manageClickCount() {
-        this.clickCount += 1;
-        if (this.clickCount > 9) {
-            this.setState({ flag: true });
+    let clickCount = useRef(0);
+
+    const manageClickCount = () => {
+        clickCount.current++;
+        if (clickCount.current > 9) {
+            setFlag(true);
         }
     }
 
-    inc() {
-        this.setState(
-            (prev) => ({ count: prev.count + this.props.interval }),
-            () => this.manageClickCount());
+    const inc = () => {
+        setCount(prev => prev + interval);
+        manageClickCount();
+    };
+
+    const dec = () => {
+        setCount(prev => prev - interval);
+        manageClickCount();
     }
 
-    dec() {
-        this.setState(
-            (prev) => ({ count: prev.count - this.props.interval }),
-            () => this.manageClickCount()
-        );
+    const reset = () => {
+        clickCount.current = 0;
+        setCount(0);
+        setFlag(false);
     }
 
-    reset() {
-        this.clickCount = 0;
-        this.setState({ count: 0, flag: false });
-    }
-
-    render() {
-        const { flag } = this.state;
-
-        return (
-            <>
-                <div className="text-center">
-                    <h3 className="text-info">Counter Component</h3>
-                </div>
-                <div className="d-grid gap-2 mx-auto col-6">
-                    <input type="text" className="form-control form-control-lg" value={this.state.count} readOnly />
-                    <CounterControls
-                        flag={flag}
-                        inc={this.inc}
-                        dec={this.dec}
-                        reset={this.reset} />
-                </div>
-            </>
-        );
-    }
+    return (
+        <>
+            <div className="text-center">
+                <h3 className="text-info">Counter Component</h3>
+            </div>
+            <div className="d-grid gap-2 mx-auto col-6">
+                <input type="text" className="form-control form-control-lg" value={count} readOnly />
+                <CounterControls
+                    flag={flag}
+                    inc={inc}
+                    dec={dec}
+                    reset={reset} />
+            </div>
+        </>
+    );
 }
 
 Counter.propTypes = {
     interval: PropTypes.number
 }
 
-Counter.defaultProps = {
-    interval: 1
-}
+const CounterControls = React.memo(({ flag, inc, dec, reset }) => {
+    console.log("CounterControls Render Executed");
 
-class CounterControls extends Component {
-    render() {
-        const { flag, inc, dec, reset } = this.props;
+    return (
+        <div className="d-grid gap-2">
+            <button className="btn btn-info" disabled={flag} onClick={inc}>
+                <span className="fs-4">+</span>
+            </button>
+            <button className="btn btn-info" disabled={flag} onClick={dec}>
+                <span className="fs-4">-</span>
+            </button>
+            <button className="btn btn-secondary" disabled={!flag} onClick={reset}>
+                <span className="fs-4">Reset</span>
+            </button>
+        </div>
+    );
+});
 
-        return (
-            <div className="d-grid gap-2">
-                <button className="btn btn-info" disabled={flag} onClick={inc}>
-                    <span className="fs-4">+</span>
-                </button>
-                <button className="btn btn-info" disabled={flag} onClick={dec}>
-                    <span className="fs-4">-</span>
-                </button>
-                <button className="btn btn-secondary" disabled={!flag} onClick={reset}>
-                    <span className="fs-4">Reset</span>
-                </button>
-            </div>
-        );
-    }
-}
-
-class CounterAssignment extends Component {
-    render() {
-        return (
-            <div>
-                <Counter />
-                <hr />
-                <Counter interval={10} />
-            </div>
-        );
-    }
+const CounterAssignment = () => {
+    return (
+        <div>
+            <Counter />
+        </div>
+    );
 }
 
 export default CounterAssignment;
