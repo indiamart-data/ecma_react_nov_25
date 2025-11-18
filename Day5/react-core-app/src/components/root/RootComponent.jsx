7f333205759a71c4bca961248fe867f768cbade1
Manish Sharma <developer@technizeredge.com>
2025-11-18 17:43:53 +0530

    Hook Form HTML Validation

--- Day5/react-core-app/src/components/root/RootComponent.jsx
+++ Day5/react-core-app/src/components/root/RootComponent.jsx
@@ -2,7 +2,8 @@
 // import CalculatorAssignment from "../2_assignment/CalculatorAssignment";
 // import ListRoot from "../3_working_with_arrays/ListComponent";
 // import CrudAssignment from "../4_assignment/CrudAssignment";
-import CrudAssignmentReducer from "../4_assignment/CrudAssignmentReducer";
+// import CrudAssignmentReducer from "../4_assignment/CrudAssignmentReducer";
+import HookFormDemo from "../5_hook_form_demo/HookFormDemo";
 import ErrorBoundary from "../common/ErrorBoundary";
 
 const RootComponent = () => {
@@ -13,7 +14,8 @@ const RootComponent = () => {
                 {/* <CalculatorAssignment />  */}
                 {/* <ListRoot /> */}
                 {/* <CrudAssignment /> */}
-                <CrudAssignmentReducer />
+                {/* <CrudAssignmentReducer /> */}
+                <HookFormDemo />
             </ErrorBoundary>
         </div>
     );
