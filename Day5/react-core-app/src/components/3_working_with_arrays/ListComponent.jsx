36cf7d7c11e865b825e28f9eba24a9dd5bf087fe
Manish Sharma <developer@technizeredge.com>
2025-11-18 12:50:04 +0530

    Working with Arrays

---
+++ Day5/react-core-app/src/components/3_working_with_arrays/ListComponent.jsx
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+
+const ListItem = ({ item }) => <li className='list-group-item'>{item.name}</li>
+
+const ListComponent = ({ items }) => {
+    return (
+        <ul className="list-group">
+            {
+                items.map((item) => {
+                    return <ListItem key={item.id} item={item} />
+                })
+            }
+        </ul>
+    );
+};
+
+const Tr = ({ item }) => {
+    return (
+        <tr>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>
+                <a href="/#" className='text-info'>Details</a>
+            </td>
+            <td>
+                <a href="/#" className='text-warning'>Edit</a>
+            </td>
+            <td>
+                <a href="/#" className='text-danger'>Delete</a>
+            </td>
+        </tr>
+    );
+}
+
+const TableComponnet = ({ items, children }) => {
+    return (
+        <>
+            {children && children}
+            <hr />
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NAME</th>
+                        <th></th>
+                        <th>ACTIONS</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        items.map((item) => {
+                            return <Tr key={item.id} item={item} />
+                        })
+                    }
+                </tbody>
+            </table>
+        </>
+    );
+};
+
+const ListRoot = () => {
+    const [employees] = useState([
+        { id: 1, name: "Manish" },
+        { id: 2, name: "Abhijeet" },
+        { id: 3, name: "Ramakant" },
+        { id: 4, name: "Subodh" },
+        { id: 5, name: "Abhishek" }
+    ]);
+
+     const [posts] = useState([
+        {
+            userId: 1,
+            id: 1,
+            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+            userId: 1,
+            id: 2,
+            title: "qui est esse",
+            body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        }
+    ]);
+
+    return (
+        <>
+            {/* <ListComponent items={employees} /> */}
+            <TableComponnet items={employees}>
+                <h3 className='text-center text-info'>Employees Table</h3>
+            </TableComponnet>
+            <TableComponnet items={posts}>
+                <h3 className='text-center text-info'>Posts Table</h3>
+            </TableComponnet>
+        </>
+    );
+};
+
+export default ListRoot;
--- Day5/react-core-app/src/components/root/RootComponent.jsx
+++ Day5/react-core-app/src/components/root/RootComponent.jsx
@@ -1,13 +1,15 @@
-import ControlledVsUncontrolledComponent from "../1_controlled-uncontrolled/ControlledVsUncontrolledComponent";
-import CalculatorAssignment from "../2_assignment/CalculatorAssignment";
+// import ControlledVsUncontrolledComponent from "../1_controlled-uncontrolled/ControlledVsUncontrolledComponent";
+// import CalculatorAssignment from "../2_assignment/CalculatorAssignment";
+import ListRoot from "../3_working_with_arrays/ListComponent";
 import ErrorBoundary from "../common/ErrorBoundary";
 
 const RootComponent = () => {
     return (
         <div className='container'>
             <ErrorBoundary>
-                <ControlledVsUncontrolledComponent />
-                <CalculatorAssignment />
+                {/* <ControlledVsUncontrolledComponent /> */}
+                {/* <CalculatorAssignment />  */}
+                <ListRoot />
             </ErrorBoundary>
         </div>
     );
