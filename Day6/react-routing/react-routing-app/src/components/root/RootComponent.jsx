deefcc2a871500229a26c657d0c9fea61e20aa09
Manish Sharma <developer@technizeredge.com>
2025-11-19 12:37:41 +0530

    React Router Start

---
+++ Day6/react-routing/react-routing-app/src/components/root/RootComponent.jsx
@@ -0,0 +1,15 @@
+import NavigationComponent from '../bs-nav/NavigationComponent';
+
+import ErrorHandler from '../common/ErrorHandler';
+
+const RootComponent = () => {
+    return (
+        <div className='container'>
+            <ErrorHandler>
+                <NavigationComponent />
+            </ErrorHandler>
+        </div>
+    );
+};
+
+export default RootComponent;
