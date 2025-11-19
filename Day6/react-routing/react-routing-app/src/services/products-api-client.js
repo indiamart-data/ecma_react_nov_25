f8292e80ad1e0c88fb67df2499498315db3ad602
Manish Sharma <developer@technizeredge.com>
2025-11-19 17:37:47 +0530

    Fetch Interceptor Added

--- Day6/react-routing/react-routing-app/src/services/products-api-client.js
+++ Day6/react-routing/react-routing-app/src/services/products-api-client.js
@@ -1,43 +1,19 @@
-import authenticatorClient from "./authenticator-api-client";
-
-const url = process.env.REACT_APP_PRODUCTS_API_URL;
-
-const productsAPIClient = {
-    getAllProducts: function () {
-        var promise = new Promise((resolve, reject) => {
-
-            let fData = {
-                method: "GET",
-                headers: {
-                    "x-access-token": authenticatorClient.getToken()
-                }
-            };
-
-            fetch(url, fData).then((res) => {
-                var result = res.json();
-                result.then((jResult) => {
-                    resolve(jResult);
-                }, (err) => {
-                    reject("JSON Parse Error");
-                });
-            }).catch((err) => {
-                console.log(err);
-                reject("Error connecting to the API");
-            });
-        });
-
-        return promise;
-    }
-}
-
-export default productsAPIClient;
+// import authenticatorClient from "./authenticator-api-client";
 
 // const url = process.env.REACT_APP_PRODUCTS_API_URL;
 
 // const productsAPIClient = {
 //     getAllProducts: function () {
 //         var promise = new Promise((resolve, reject) => {
-//             fetch(url).then((res) => {
+
+//             let fData = {
+//                 method: "GET",
+//                 headers: {
+//                     "x-access-token": authenticatorClient.getToken()
+//                 }
+//             };
+
+//             fetch(url, fData).then((res) => {
 //                 var result = res.json();
 //                 result.then((jResult) => {
 //                     resolve(jResult);
@@ -55,3 +31,27 @@ export default productsAPIClient;
 // }
 
 // export default productsAPIClient;
+
+const url = process.env.REACT_APP_PRODUCTS_API_URL;
+
+const productsAPIClient = {
+    getAllProducts: function () {
+        var promise = new Promise((resolve, reject) => {
+            fetch(url).then((res) => {
+                var result = res.json();
+                result.then((jResult) => {
+                    resolve(jResult);
+                }, (err) => {
+                    reject("JSON Parse Error");
+                });
+            }).catch((err) => {
+                console.log(err);
+                reject("Error connecting to the API");
+            });
+        });
+
+        return promise;
+    }
+}
+
+export default productsAPIClient;
